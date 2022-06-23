2866ccceda5d455c93b6f19380eaa04cdd584a28
Mover12 <76180380+Mover12@users.noreply.github.com>
2022-06-23 14:33:00 +0300

    master

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,6 @@
 
 export default function Chat(){
     return(
-<<<<<<< HEAD
-        <div>kdlnflkscvklz</div>
-=======
         <div>Chat</div>
->>>>>>> 55882fdc77458fedc438abfb51dc30054224679d
     );
 }
