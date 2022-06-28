86267dd1e30eb02c438438bd87c088c446279a43
LevZhigalin1 <108064453+LevZhigalin1@users.noreply.github.com>
2022-06-28 18:59:41 +0300

    Update Chat.jsx

--- src/pages/componets/Chat.jsx
+++ src/pages/componets/Chat.jsx
@@ -1,43 +1,42 @@
-import React, { useState,  useEffect } from 'react'
-import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore'
-import { db, auth } from "../../config-firebase.js"
-//import MessageBox from './MessageBox.jsx'
-import MessageForm from './MessageForm.jsx'
+import React, { useState,  useEffect } from "react";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+import { db, auth } from "../../config-firebase.js";
+//import MessageBox from './MessageBox.jsx' пока не знаю как это сделать 
+import MessageForm from "./MessageForm.jsx";
 
 export default function Chat() {
     const [text, setText] = useState('');
+    const [UsersData, setUsersData] = useState();
     const user = auth.currentUser;
-    const [UsersData, setUsersData] = useState()
-    const q = query(collection(db, "messages"), orderBy("createdAt"))
+    const col = collection(db, "messages");  
+    const q = query(col, orderBy("createdAt"));
 
     useEffect(() => {
-        const getData = async () => await onSnapshot(q, collection(db, "messages"), (doc) => {
+        const getData = async () => await onSnapshot(q, col, (doc) => {
             setUsersData(doc.docs.map((doc) => ({
                 id:doc.id,
                 ...doc.data()
             })))
         })
-        getData()
-        console.log(UsersData)
+        getData();
     }, [])
 
     const addMessage = () => {
-        if (text !== ""){
-            addDoc(collection(db,'messages'), {
+        if(text !== ""){
+            addDoc(col, {
                 name: user.displayName,
                 text: text,
                 createdAt: serverTimestamp(),
             })
             setText("");
         }
-    }//await ага ждать, ждать чего отправки сообщения?
-
+    }
     
     return (
-        <div>
-            <div className='messages'>
+        <div className="chat">
+            <div className="messages">
                 {UsersData?.map((user) => 
-                    <div key={user.id}>
+                    <div key={user.id} className="message">
                         <div>{user.name}: {user.text}</div>
                     </div>
                 )}
