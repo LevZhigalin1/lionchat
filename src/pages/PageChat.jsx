9ab1568af2783516d9af8d2210d296577efa3a3c
LevZhigalin1 <nonenothing@mail.ru>
2022-07-04 23:41:06 +0300

    master

--- src/pages/PageChat.jsx
+++ src/pages/PageChat.jsx
@@ -3,16 +3,22 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../config-firebase.js";
 import useAuthState from "../hooks/useAuthState";
 import Chat from "./componets/Chat";
+import { useCookies } from "react-cookie";
 
 export default function PageChat(){
     const isAuth = useAuthState(auth)
+    const [cookies, setCookie ] = useCookies();
 
     return(
         <div>
             { isAuth ? 
                 <Chat />
                 :
-                <button onClick={() => signInWithPopup(auth, provider)}>Авторизация через Google</button>
+                <button onClick={() => 
+                    {signInWithPopup(auth, provider);
+                    setCookie("nick", "Инкогнито");
+                    setCookie("color", "#497fab");}
+                }>Авторизация через Google</button>
             }
         </div>
     );
